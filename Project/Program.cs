e68dca4ca798b46153aa0f6b21166b0b42d44ade
vnlfltmxm <163379543+vnlfltmxm@users.noreply.github.com>
2024-03-15 13:16:57 +0900

    Project

---
+++ Project/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Filed f = new Filed(30, 50);
+
+            f.PrintFiled();
+            
+        }
+    }
+}
